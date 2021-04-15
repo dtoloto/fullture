e24e9cba635ad301415e43a8cd1f238261dafd18
Daniel Toloto <dctoloto@gmail.com>
2021-04-15 19:01:06 -0300

    add: imc calculator

--- 14-04-21/calculadora-imc/scripts.js
+++ 14-04-21/calculadora-imc/scripts.js
@@ -3,10 +3,37 @@ const calcularIMC = (event) => {
   const inputPeso = document.querySelector('#peso');
   const inputAltura = document.querySelector('#altura');
 
+  const resposta = document.querySelector('#resposta');
+
   const peso = inputPeso.value;
+  console.log(`Peso: ${peso}`);
   const altura = inputAltura.value;
+  console.log(`Altura: ${altura}`);
+
+  inputPeso.classList.remove('error');
+  inputAltura.classList.remove('error');
+  resposta.classList.remove('resposta-error');
+
+  let error = false;
+
+  if (!peso) {
+    inputPeso.classList.add('error');
+    error = true;
+  }
+
+  if (!altura) {
+    inputAltura.classList.add('error');
+    error = true;
+  }
+
+  if (error) {
+    resposta.classList.add('resposta-error');
+    resposta.innerHTML = 'Por favor, preencha todos os campos!'
+    return;
+  }
 
   const imc = peso / (altura * altura);
+  console.log(`IMC: ${imc}`);
 
-  alert(`O seu IMC é: ${imc}`);
+  resposta.innerHTML = `O seu IMC é: ${imc.toFixed(2)}`;
 }
