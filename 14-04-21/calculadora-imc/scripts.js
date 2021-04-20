c3f69e9e199821bfe526d4d0649b0e5276d61c95
Daniel Toloto <dctoloto@gmail.com>
2021-04-20 18:58:08 -0300

    add: male calculator

--- 14-04-21/calculadora-imc/scripts.js
+++ 14-04-21/calculadora-imc/scripts.js
@@ -1,14 +1,17 @@
 const calcularIMC = (event) => {
   event.preventDefault();
+
   const inputPeso = document.querySelector('#peso');
   const inputAltura = document.querySelector('#altura');
   const inputNome = document.querySelector('#nome');
+  const inputGenero = document.querySelector('input[name="genero"]:checked');
 
   const resposta = document.querySelector('#resposta');
 
   const peso = inputPeso.value;
   const altura = inputAltura.value;
   const nome = inputNome.value;
+  const genero = inputGenero.value;
 
   inputPeso.classList.remove('error');
   inputAltura.classList.remove('error');
@@ -26,6 +29,11 @@ const calcularIMC = (event) => {
     error = true;
   }
 
+  if (!nome) {
+    inputNome.classList.add('error');
+    error = true;
+  }
+
   if (error) {
     resposta.classList.add('resposta-error');
     resposta.innerHTML = 'Por favor, preencha todos os campos!'
@@ -34,10 +42,27 @@ const calcularIMC = (event) => {
 
   const imc = peso / (altura * altura);
 
-  resposta.innerHTML = `O seu IMC é: ${imc.toFixed(2)}kg/m²`;
+  let situacao;
+
+  if (genero === 'homem') {
+    if (imc >= 40) {
+      situacao = 'Obesidade Mórbida';
+    } else if (imc >= 30 && imc < 40) {
+      situacao = 'Obesidade Mórbida'
+    } else if (imc >= 25 && imc < 30) {
+      situacao = 'Obesidade Leve'
+    } else if (imc >= 20 && imc < 25) {
+      situacao = 'Normal'
+    } else {
+      situacao = 'Abaixo do Normal'
+    }
+  }
+
+
+  resposta.innerHTML = `O seu IMC é: ${imc.toFixed(2)}kg/m² (${situacao})`;
 
   const item = document.createElement('LI');
-  item.innerHTML = `${nome} - ${imc.toFixed(2)}kg/m²`;
+  item.innerHTML = `${nome} - ${imc.toFixed(2)}kg/m² - ${situacao}`;
 
   const lista = document.querySelector('#lista');
   lista.appendChild(item);
